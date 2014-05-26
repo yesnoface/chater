e89d30ded961e41109479e3a77cec610f5613071
Yana Rain <yesnoface@gmail.com>
2014-05-26 13:06:35 +0400

    Message between popup and background

---
+++ background.js
@@ -0,0 +1,26 @@
+console.log('worked');
+
+//get ip
+var ipList = {};
+chrome.webRequest.onCompleted.addListener(
+	function(info) {
+		ipList[info.url] = info.ip;
+		ipList[info.tabId] = ipList[info.tabId] || [];
+		ipList[info.tabId].push(info);
+
+		chrome.extension.sendRequest(
+			{ip: info.ip, url: info.url}, 
+			function(response) {
+
+			}
+		);
+
+		console.log(info.ip);
+		return;
+	},
+	{
+		urls: [],
+		types: []
+	},
+	[]
+);
--- popup.js
+++ popup.js
@@ -1,3 +1,12 @@
-send.addEventListener('click', function() {
-	send.innerHTML = text.value;
-});
+chrome.extension.onRequest.addListener(
+	function(request, sender, sendResponse) {
+		ip.innerHTML = request.ip + ' ' + request.url;
+	}
+);
+
+send.addEventListener(
+	'click',
+	function() {
+		send.innerHTML = text.value;
+	}
+);
